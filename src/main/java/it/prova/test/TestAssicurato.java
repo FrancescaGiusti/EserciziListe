120e2be52fe20bc50ce193c95bfb28b055c6a1c8
FrancescaGiusti <francesca.pescia01@gmail.com>
2025-12-04 17:30:37 +0100

    Esercizio Assicurato

--- src/main/java/it/prova/test/TestAssicurato.java
+++ src/main/java/it/prova/test/TestAssicurato.java
@@ -1,4 +1,65 @@
 package it.prova.test;
 
+import it.prova.utility.Assicurato;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TestAssicurato {
+    public static void main(String[] args){
+        //Test per vedere se un assicurato è presente nella lista
+        List<Assicurato> assicurati = new ArrayList<>(Arrays.asList(new Assicurato("Mario", "Rossi", "MSGET543MN745", 158), new Assicurato("Gaia", "Bianchi", "SAFSG63523SFGT6", 200)));
+        Assicurato cliente = new Assicurato("Mario", "Rossi", "MSGET543MN745", 158);
+        Assicurato cliente2 = new Assicurato("Matteo", "Ricci", "FASDJSFY53427F", 130);
+        boolean result1 = cliente2.presenteInElenco(assicurati);
+        System.out.println("La persona cercata è presente in elenco? " + result1);
+
+        //Test per vedere quanti assicurati hanno il cognome che inizia una certa lettera
+        List<Assicurato> assicurati2 = new ArrayList<>(Arrays.asList(new Assicurato("Mario", "Rossi", "MSGET543MN745", 158), new Assicurato("Gaia", "Bianchi", "SAFSG63523SFGT6", 200)));
+        assicurati2.add(new Assicurato("Matteo", "Ricci", "FASDJSFY53427F", 130));
+        int result2 = quantiConInizialeNelCognome(assicurati2, 'R');
+        System.out.println("Quanti assicurati hanno il cognome che inizia con la stessa lettera? " + result2);
+
+        //Test per estrapolare la lista di cognomi degli assicurati
+        List<Assicurato> assicurati3 = new ArrayList<>(Arrays.asList(new Assicurato("Mario", "Rossi", "MSGET543MN745", 158), new Assicurato("Gaia", "Bianchi", "SAFSG63523SFGT6", 200), new Assicurato("Matteo", "Ricci", "FASDJSFY53427F", 130)));
+        List<String> cognomi = estraiSoloICognomi(assicurati3);
+        System.out.println("Ecco la lista di cognomi: " + cognomi);
+
+        //Test per estrarre gli assicurati con rata maggiore di un certo input
+        List<Assicurato> assicurati4 = new ArrayList<>(Arrays.asList(new Assicurato("Mario", "Rossi", "MSGET543MN745", 158), new Assicurato("Gaia", "Bianchi", "SAFSG63523SFGT6", 200), new Assicurato("Matteo", "Ricci", "FASDJSFY53427F", 130)));
+        List<Assicurato> assicuratiConRataMaggiore = estraiQuelliConRataMaggioreDi(assicurati4, 80);
+        System.out.println("Ecco la lista degli assicurati con rata maggiore di quella indicata: " + assicuratiConRataMaggiore.toString());
+    }
+
+
+    static int quantiConInizialeNelCognome(List<Assicurato> assicurati, char iniziale){
+        int result = 0;
+        for (int i = 0; i < assicurati.size(); i++){
+            if (assicurati.get(i).getCognome().charAt(0) == iniziale){
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public static List<String> estraiSoloICognomi(List<Assicurato> assicurati){
+        List<String> cognomi = new ArrayList<>();
+        for (int i = 0; i < assicurati.size(); i++){
+            cognomi.add(assicurati.get(i).getCognome());
+        }
+        return cognomi;
+    }
+
+    public static List<Assicurato> estraiQuelliConRataMaggioreDi(List<Assicurato> assicurati, int sogliaRata){
+        List<Assicurato> result = new ArrayList<>();
+        for(int i = 0; i < assicurati.size(); i++){
+            if (assicurati.get(i).getRataPremioAssicurativo() > sogliaRata){
+                result.add(assicurati.get(i));
+            }
+        }
+        return result;
+    }
+
+
 }
--- src/main/java/it/prova/utility/Assicurato.java
+++ src/main/java/it/prova/utility/Assicurato.java
@@ -1,4 +1,77 @@
 package it.prova.utility;
 
+import java.util.List;
+
 public class Assicurato {
+    private String nome;
+    private String cognome;
+    private String codiceFiscale;
+    private int rataPremioAssicurativo;
+
+    public Assicurato(){}
+
+    public Assicurato(String nome, String cognome, String codiceFiscale, int rataPremioAssicurativo){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.codiceFiscale = codiceFiscale;
+        this.rataPremioAssicurativo = rataPremioAssicurativo;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return this.cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getCodiceFiscale() {
+        return this.codiceFiscale;
+    }
+
+    public void setCodiceFiscale(String codiceFiscale) {
+        this.codiceFiscale = codiceFiscale;
+    }
+
+    public int getRataPremioAssicurativo() {
+        return this.rataPremioAssicurativo;
+    }
+
+    public void setRataPremioAssicurativo(int rataPremioAssicurativo) {
+        this.rataPremioAssicurativo = rataPremioAssicurativo;
+    }
+
+    public boolean presenteInElenco(List<Assicurato> assicurati){
+        for (int i = 0; i < assicurati.size(); i++){
+            if(this.equals(assicurati.get(i))){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o){
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()){
+            return false;
+        }
+        Assicurato a = (Assicurato)o;
+        return this.getNome().equalsIgnoreCase(a.getNome()) && this.getCognome().equalsIgnoreCase(a.getCognome()) && this.getCodiceFiscale().equalsIgnoreCase(a.getCodiceFiscale()) && this.getRataPremioAssicurativo() == a.getRataPremioAssicurativo();
+    }
+
+    @Override
+    public String toString(){
+        return "Nome e Cognome: " + this.nome + " " + this.cognome + ", codice fiscale: " + this.codiceFiscale + ", rata assicurativa: " + this.rataPremioAssicurativo + " euro";
+    }
 }
